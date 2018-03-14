33d8be245d99ba5c00615aa6a954c0491e336a01
daimx <17098906419@163.com>
2018-03-14 12:40:49 +0800

    完善今日头条街拍图片爬虫

--- ToutiaoSpider/__init__.py
+++ ToutiaoSpider/__init__.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 # -*- coding:utf-8 -*-
+import logging
 import os
 import requests
 from urllib.parse import urlencode
+from hashlib import md5
+from multiprocessing.pool import Pool
 
 
 def get_page(offset):
     params = {
-        'offset': offset
+        'offset': offset,
         'format': 'json',
         'keyword': '街拍',
         'autoload': 'true',
@@ -15,23 +18,61 @@ def get_page(offset):
         'cur_tab': '3',
         'from': 'gallery'
     }
-    url = 'https://www.toutiao.com/search/?' + urlencode(params)
+    url = 'https://www.toutiao.com/search_content/?' + urlencode(params)
     try:
         response = requests.get(url)
         if response.status_code == 200:
             return response.json()
-    except requests.ConnectionError:
+    except requests.ConnectionError as e:
+        e.args
         return None
+
 
 def get_images(json):
     if json.get('data'):
         for item in json.get('data'):
             title = item.get('title')
-            images = item.get('image_list')
+            images = item.get('image_detail')
+            for image in images:
+                yield{
+                    'image': image.get('url'),
+                    'title': title
+                }
 
 
 def save_image(item):
     if not os.path.exists(item.get('title')):
+        # 创建文件夹
+        logging.info('文件夹{0}创建成功!!!'.format(item.get('title')))
         os.mkdir(item.get('title'))
     try:
         response = requests.get(item.get('image'))
+        if response.status_code == 200:
+            file_path = '{0}/{1}.{2}'.format(item.get('title'), md5(response.content).hexdigest(), 'jpg')
+            if not os.path.exists(file_path):
+                with open(file_path, 'wb') as f:
+                    logging.info('文件{0}写入成功!!!'.format(file_path))
+                    f.write(response.content)
+            else:
+                print('Already Downloaded', file_path)
+    except requests.ConnectionError:
+        print('Failed to Save Image')
+
+
+def main(offset):
+    json = get_page(offset)
+    for item in get_images(json):
+        print(item)
+        save_image(item)
+
+
+GROP_START = 1
+GROP_END = 20
+
+
+if __name__ == '__main__':
+    pool = Pool()
+    groups = ([x * 20 for x in range(GROP_START, GROP_END + 1)])
+    pool.map(main, groups)
+    pool.close()
+    pool.join()
